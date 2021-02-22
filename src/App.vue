d3bfa39ca2a40128fd201d8f9cec63b585af335b
hanguoshuai <756640930@qq.com>
2021-02-22 13:45:58 +0800

    2021.0222提交初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <tab-bar>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+      <tab-bar-item></tab-bar-item>
+    </tab-bar>
   </div>
 </template>
 
 <script>
+
+import TabBar from '@/components/common/TabBar.vue'
+import TabBarItem from '@/components/common/tabBarItem.vue'
+
+  
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TabBar,
+    TabBarItem
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    
 </style>
---
+++ src/components/common/tabBarItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="tab-bar-item">
+    <img src="@/assets/img/tabbar/home.svg" alt="" />
+    <div>
+      首页
+    </div>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "tabBarItem"
+	}
+</script>
+
+<style scoped>
+  .tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+  }
+
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    /* 图片下默认有三像素间隔（去掉） */
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+</style>
+
+
